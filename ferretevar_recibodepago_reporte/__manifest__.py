d48fe308dcf95c233a49616f641fa577e04c01f2
MaryCruzPalalia <mpm@soluciones4g.com>
2019-07-23 16:47:45 -0500

    [ADD]módulo  que quita el duplicado de datos de recibo de pago

---
+++ ferretevar_recibodepago_reporte/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name' : "reporte de recibo de pago ferretevar",
+    'version' : "1.0",
+    'description' : "Quitar duplicado de datos en recibo de pago",
+    'author' : "Soluciones4g", 
+    'depends' : ['account',],
+    'data': [
+    	'views/report_recibo_pago.xml',
+    	
+        
+    ],
+    'installable' : True,
+}
