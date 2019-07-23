95410fd15ff4092668c335ce78d85fe359b48603
MaryCruzPalalia <mpm@soluciones4g.com>
2019-07-23 16:46:43 -0500

    [ADD]módulo  que quitael duplicado dela informacion en el reporte de factura

---
+++ reporte_factura_duplicado_info/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name' : "reporte de factura quitar fields",
+    'version' : "1.0",
+    'description' : "Quitar duplicado de datos en reporte de factura",
+    'author' : "Soluciones4g", 
+    'depends' : ['account',],
+    'data': [
+    	'views/report_factura.xml',
+    	
+        
+    ],
+    'installable' : True,
+}
